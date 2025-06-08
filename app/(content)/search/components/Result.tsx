e8b8a7a0b3d3c34ee15fc75eb7d8c4a116fcd7e4
AnasHbaieb <anashbaieb@gmail.com>
2025-06-08 12:57:56 +0100

    some fix error

--- app/(content)/search/components/Result.tsx
+++ app/(content)/search/components/Result.tsx
@@ -21,7 +21,7 @@ export default function Result({ filteredExperts }: ResultProps) {
           </p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {filteredExperts.map((expert) => (
             <ExpertCard key={expert.id} expert={expert} />
           ))}
